b6ebca83a7a5bb2654e1318a4ce6922ae5652dce
Fred Loney <loneyf@ohsu.edu>
2012-04-02 15:18:37 -0700

    Move model to spec support.

--- spec/support/model.rb
+++ spec/support/model.rb
@@ -1,6 +1,5 @@
 # Load the jinx model example.
-
-require Bundler.environment.specs.detect { |s| s.name == 'jinx' }.full_gem_path + '/test/helpers/model'
+require Bundler.environment.specs.detect { |s| s.name == 'jinx' }.full_gem_path + '/spec/support/model'
 
 # Make the test domain classes migratable
 module Domain
