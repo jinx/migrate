bcc702be752d8201b051d6b5afa25c319ee76df4
Fred Loney <loneyf@ohsu.edu>
2012-04-13 13:35:26 -0700

    Clean up specs.

--- test/helpers/clinical_trials.rb
+++
@@ -1,4 +0,0 @@
-require File.dirname(__FILE__) + '/../helper'
-
-# Load the jinx clinical trials example.
-require Bundler.environment.specs.detect { |s| s.name == 'jinx' }.full_gem_path + '/examples/clinical_trials/lib/clinical_trials'
